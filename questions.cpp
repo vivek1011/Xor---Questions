fd9b4bbab45e30acaad2a5c7cf8150ceffb2baab
Vivek Bansal <bansalvivek79@gmail.com>
2016-03-15 18:14:00 +0530

    Update questions.cpp

--- questions.cpp
+++ questions.cpp
@@ -1,2 +1,9 @@
-https://www.hackerrank.com/contests/hourrank-1/challenges/xor-groups
-calculate number of non empty (two) disjoint sets with equal xor value  
+XMAX spoj - find maximum XOR over all the subsets 
+XORSUB - has to take one value and find maximum over all the subsets.
+XORSACK (IPC) - check if a subset exists with given XOR . 
+all based on same strategy- try to maximise the msb set in all cases using vector[bit] .
+solution -  https://www.codechef.com/viewsolution/9676294
+
+Find no of ways of having Given Xor(over all subsets)- dp[ith position tak][particular Xor] = no of ways.
+
+
